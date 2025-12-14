0ce3a08889a4f5bd2b829d6edaefd5b42152bb08
buzzin5113 <buzzin@mail.ru>
2025-12-14 19:24:59 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,5 @@
 DEBUG = True
 # Пауза между запросами обновлений в telegram
 UPDATE_TIMER = 20
-# Список пользователей которые могут отправлять команды
-USERS = ['alexeytimofeew', 'domagate_bot']
 # Использовать GitHub для хранения списка доменов
 USE_GITHUB = False
